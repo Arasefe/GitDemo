a6fba7a1b86a1e027a3c15cab71fd4f59c1d427d
Arasefe <ismail_yildirim69@yahoo.com>
2020-08-01 10:52:08 -0500

    add HappyB20.java

--- src/HappyB20.java
+++ src/HappyB20.java
@@ -1,4 +1,5 @@
 public class HappyB20 {
+    static int salary=190000;
     public static void main(String[] args) {
         System.out.println("I am happy with my instructor");
         happyMethod(178000);
@@ -6,5 +7,9 @@ public class HappyB20 {
     public static void happyMethod(int salary){
         boolean isHappy=(salary>150000)?true:false;
         System.out.println(isHappy);
+    }
+    public static int happyMethod(String name){
+        boolean isHappy=(name.equals("Aras"))?true:(name.equals("Tulpar"))?true:false;
+        return salary;
     }
 }
