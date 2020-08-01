a1ac1c15838509b4a17e8cdbe13993c3fd50a508
Arasefe <ismail_yildirim69@yahoo.com>
2020-08-01 15:03:04 -0500

    Update BookARoom.java

--- src/BookARoom.java
+++ src/BookARoom.java
@@ -1,5 +1,8 @@
 public class BookARoom {
     public static void main(String[] args) {
+        methodAdd("Aras");
         System.out.println("I am booking a room");
     }
+    public static int methodAdd(String name){
+        String result=(name.equals("Aras"))?"happiness":"unhappiness";
 }
