f7feac5c3057a3f971ce449588f8f716ac00654c
Arasefe <ismail_yildirim69@yahoo.com>
2020-08-01 12:06:20 -0500

    add MyLife.java

--- src/MyLife.java
+++ src/MyLife.java
@@ -2,5 +2,7 @@ public class MyLife {
     public static void main(String[] args) {
         System.out.println("I love my family");
     }
-
+    public static void methodMyLife(){
+        System.out.println("Aras and Tulpar");
+    }
 }
