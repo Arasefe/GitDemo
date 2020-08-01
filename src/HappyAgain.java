d6f1b551cbcd6efa8f0e99bf9c0c1ded6acaa238
Arasefe <ismail_yildirim69@yahoo.com>
2020-08-01 11:51:17 -0500

    add HappyAgain.java

---
+++ src/HappyAgain.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class HappyAgain {
+    public static void main(String[] args) {
+        main(new int[]{1,2,3,4,5});
+
+    }
+    public static void main(int[]arr){
+        System.out.println(Arrays.toString(arr));
+
+    }
+}
