00f7ccf37532e885d62f73aa83d416293097623a
Arasefe <ismail_yildirim69@yahoo.com>
2020-08-01 12:26:12 -0500

    add Demo.java

--- src/Demo.java
+++ src/Demo.java
@@ -1,18 +1,1 @@
-public class Demo {
-    public static void main(String[] args) {
-        System.out.println("First creation");
-        methodAd(3,5);
-        swap(3,4);
-    }
-    public static void methodAd(int a, int b){
-        int result=a+b;
-    }
-    public static void swap(int a, int b){
-        int temp=0;
-        temp=a;
-        a=b;
-        b=temp;
-        System.out.println(a);
-        System.out.println(b);
-    }
-}
+public class Demo {    public static void main(String[] args) {        System.out.println("First creation");        methodAd(3,5);        swap(3,4);    }    public static void methodAd(int a, int b){        int result=a+b;    }    public static void swap(int a, int b){        int temp=0;        temp=a;        a=b;        b=temp;        System.out.println(a);        System.out.println(b);    }}
